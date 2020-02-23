29b15f233defcdad8b05348f727acaac01cd63a1
Волков Алексей <wolf.alex.1997@mail.ru>
2020-02-23 15:26:30 +0300

    version 6

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [   
     path("", views.index, name="index"),
-    path("group/<slug:slug>/", views.group_posts, name="group"),
+    path("group/<slug>/", views.group_posts, name="group"),
     path("new/", views.new_post, name="new_post"),
 ]
